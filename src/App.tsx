956dc33791a8a9202e01bb5678c049d6a15d5932
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-12-23 17:34:15 -0800

    more

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,69 @@
-import type { Component } from "solid-js";
+import * as v from "@badrap/valita";
+import { HashRouter, Route, useLocation, useNavigate, useParams } from "@solidjs/router";
+import { type Component, createResource, createSignal, Match, Show, Switch } from "solid-js";
+
+const Response = v.object({
+    data: v.object({
+        lists: v.array(v.object({
+            created_at: v.string(),
+            did: v.string(),
+            url: v.string(),
+            name: v.string(),
+            description: v.string(),
+        })),
+    }),
+});
+
+const fetchLists = async (handle: string) => {
+    const response = await fetch(`https://api.clearsky.services/api/v1/anon/get-list/${encodeURIComponent(handle)}`);
+    const json = await response.json();
+    return Response.parse(json, { mode: "strip" });
+};
+
+const Page: Component = () => {
+    const navigate = useNavigate();
+    const params = useParams<{ handle?: string | undefined; }>();
+
+    return (
+        <div>
+            <input
+                placeholder="Enter handle"
+                onInput={(e) => navigate("/" + e.currentTarget.value)}
+            />
+            <p>hello {params.handle}</p>
+        </div>
+    );
+};
 
 const App: Component = () => {
     return (
-        <p class="text-4xl text-green-700 text-center py-20">
-            Hello{" "}
-            <a
-                class="text-pink-600 hover:font-bold hover:border-1"
-                href="https://antfu.me/posts/reimagine-atomic-css"
-                target="atomic-css"
-            >
-                Atomic CSS
-            </a>
-            !
-        </p>
+        <HashRouter root={(props) => <>{props.children}</>}>
+            <Route path="/:handle?" component={Page} />
+        </HashRouter>
     );
+
+    // const [handle, setHandle] = createSignal<string | undefined>();
+    // const [lists] = createResource(handle, fetchLists);
+
+    // return (
+    //     <div>
+    //         <input
+    //             placeholder="Enter handle"
+    //             onInput={(e) => setHandle(e.currentTarget.value)}
+    //         />
+    //         <Show when={lists.loading}>
+    //             <p>Loading...</p>
+    //         </Show>
+    //         <Switch>
+    //             <Match when={lists.error}>
+    //                 <span>Error: {JSON.stringify()}</span>
+    //             </Match>
+    //             <Match when={lists()}>
+    //                 <div>{JSON.stringify(lists())}</div>
+    //             </Match>
+    //         </Switch>
+    //     </div>
+    // );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import "uno.css";
 import { render } from "solid-js/web";
 
 import App from "./App";
