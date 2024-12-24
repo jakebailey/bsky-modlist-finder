26829b5c1ce59a8f124c9bfb1aeb1187a46848e2
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-12-23 18:56:04 -0800

    nice

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import "water.css";
+import "./App.css";
+
 import * as v from "@badrap/valita";
-import { HashRouter, Route, useLocation, useNavigate, useParams } from "@solidjs/router";
-import { type Component, createResource, createSignal, Match, Show, Switch } from "solid-js";
+import { HashRouter, Route, useNavigate, useParams } from "@solidjs/router";
+import { type Component, createResource, Match, Show, Switch } from "solid-js";
 
 const Response = v.object({
     data: v.object({
         lists: v.array(v.object({
-            created_at: v.string(),
+            created_at: v.string().nullable().optional(),
             did: v.string(),
             url: v.string(),
             name: v.string(),
-            description: v.string(),
+            description: v.string().nullable().optional(),
         })),
     }),
 });
@@ -17,19 +20,42 @@ const Response = v.object({
 const fetchLists = async (handle: string) => {
     const response = await fetch(`https://api.clearsky.services/api/v1/anon/get-list/${encodeURIComponent(handle)}`);
     const json = await response.json();
-    return Response.parse(json, { mode: "strip" });
+    try {
+        return Response.parse(json, { mode: "strip" });
+    } catch (e) {
+        console.error(json);
+        throw e;
+    }
 };
 
 const Page: Component = () => {
     const navigate = useNavigate();
     const params = useParams<{ handle?: string | undefined; }>();
+    const [lists] = createResource(params.handle || undefined, fetchLists);
 
     return (
         <div>
-            <input
-                placeholder="Enter handle"
-                onInput={(e) => navigate("/" + e.currentTarget.value)}
-            />
+            <h1>Bluesky Moderation List Finder</h1>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    navigate(`/${(e.currentTarget.handle as HTMLInputElement).value}`);
+                }}
+            >
+                <input id="handle" placeholder="Enter handle" />
+                <button type="submit">Submit</button>
+            </form>
+            <Show when={lists.loading}>
+                <p>Loading...</p>
+            </Show>
+            <Switch>
+                <Match when={lists.error}>
+                    <span>Error: {`${lists.error}`}</span>
+                </Match>
+                <Match when={lists()}>
+                    <div>{JSON.stringify(lists())}</div>
+                </Match>
+            </Switch>
             <p>hello {params.handle}</p>
         </div>
     );
@@ -41,29 +67,6 @@ const App: Component = () => {
             <Route path="/:handle?" component={Page} />
         </HashRouter>
     );
-
-    // const [handle, setHandle] = createSignal<string | undefined>();
-    // const [lists] = createResource(handle, fetchLists);
-
-    // return (
-    //     <div>
-    //         <input
-    //             placeholder="Enter handle"
-    //             onInput={(e) => setHandle(e.currentTarget.value)}
-    //         />
-    //         <Show when={lists.loading}>
-    //             <p>Loading...</p>
-    //         </Show>
-    //         <Switch>
-    //             <Match when={lists.error}>
-    //                 <span>Error: {JSON.stringify()}</span>
-    //             </Match>
-    //             <Match when={lists()}>
-    //                 <div>{JSON.stringify(lists())}</div>
-    //             </Match>
-    //         </Switch>
-    //     </div>
-    // );
 };
 
 export default App;
