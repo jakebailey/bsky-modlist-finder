d3d60fde8fb18d9e8371fa60c139f355eda602bd
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-12-24 19:46:04 -0800

    more

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "water.css";
 import "./App.css";
 
-import { HttpClient, HttpClientResponse } from "@effect/platform";
+import { FetchHttpClient, HttpClient, HttpClientResponse } from "@effect/platform";
 import { BrowserRuntime } from "@effect/platform-browser";
 import { HashRouter, Route, useNavigate, useParams } from "@solidjs/router";
-import { Effect, Schema } from "effect";
+import { Effect, Either, Schema } from "effect";
 import { type Component, createResource, For, Match, Show, Switch } from "solid-js";
 
 const ClearskyListsSchema = Schema.Struct({
@@ -23,7 +23,15 @@ const getClearskyLists = (handle: string) =>
         const client = yield* HttpClient.HttpClient;
         const response = yield* client.get(`https://api.clearsky.services/api/v1/anon/get-list/${handle}`);
         const lists = yield* HttpClientResponse.schemaBodyJson(ClearskyListsSchema)(response);
-        return lists.data.lists;
+
+        const seen = new Set<string>();
+        return lists.data.lists.filter((list) => {
+            if (seen.has(list.url)) {
+                return false;
+            }
+            seen.add(list.url);
+            return true;
+        });
     });
 
 const BlueskyListsSchema = Schema.Struct({
@@ -32,22 +40,14 @@ const BlueskyListsSchema = Schema.Struct({
     }),
 });
 
-const getBlueskyList = (list: typeof ClearskyListsSchema.Type.data.lists[number]) =>
+const getBlueskyList = (did: string, url: string) =>
     Effect.gen(function*() {
         const client = yield* HttpClient.HttpClient;
-        const id = list.url.split("/").at(-1);
-        const at = `at://${list.did}/app.bsky.lists/${id}`;
+        const id = url.split("/").at(-1);
+        const at = `at://${did}/app.bsky.lists/${id}`;
         const response = yield* client.get(`https://public.api.bsky.app/xrpc/app.bsky.graph.getList?list=${at}`);
         const json = yield* HttpClientResponse.schemaBodyJson(BlueskyListsSchema)(response);
-        if (json.list.purpose !== "moderation") return undefined;
-        return { clearsky: list, bluesky: json.list };
-    });
-
-const getBlueskyLists = (lists: typeof ClearskyListsSchema.Type.data.lists) =>
-    Effect.gen(function*() {
-        const [errors, resultsOrUndefined] = yield* Effect.partition(lists, getBlueskyList);
-        const results = resultsOrUndefined.filter((list) => list !== undefined);
-        return { errors, results };
+        return json.list;
     });
 
 const BlueskyProfileSchema = Schema.Struct({
@@ -67,24 +67,55 @@ const getBlueskyProfile = (handle: string) =>
     });
 
 // handle should already be URL safe
-// const doWork = (handle: string) =>
-//     Effect.gen(function*() {
-//         const client = yield* HttpClient.HttpClient;
-//         const clearskyResponse = yield* client.get(`https://api.clearsky.services/api/v1/anon/get-list/${handle}`);
-//         const clearskyJson = yield* HttpClientResponse.schemaBodyJson(ClearskyListsSchema)(clearskyResponse);
-//         const clearskyLists = clearskyJson.data.lists;
+const doWork = (queryHandle: string) =>
+    Effect.gen(function*() {
+        const profile = yield* getBlueskyProfile(queryHandle);
+        const clearskyLists = yield* getClearskyLists(queryHandle);
 
-//     });
+        const lists = [];
+        const blueskyErrors = [];
+
+        for (const list of clearskyLists) {
+            const eitherBlueskyList = yield* Effect.either(getBlueskyList(list.did, list.url));
+            if (Either.isLeft(eitherBlueskyList)) {
+                blueskyErrors.push(eitherBlueskyList.left);
+                continue;
+            }
+
+            const blueskyList = eitherBlueskyList.right;
+            if (blueskyList.purpose !== "app.bsky.graph.defs#modlist") {
+                continue;
+            }
+
+            const eitherBlueskyProfile = yield* Effect.either(getBlueskyProfile(list.did));
+            if (Either.isLeft(eitherBlueskyProfile)) {
+                blueskyErrors.push(eitherBlueskyProfile.left);
+                continue;
+            }
+
+            const blueskyProfile = eitherBlueskyProfile.right;
+            lists.push({
+                profile: blueskyProfile,
+                list,
+            });
+        }
+
+        // sort descending by followers count
+        lists.sort((a, b) => b.profile.followersCount - a.profile.followersCount);
+
+        return { profile, lists, blueskyErrors };
+    });
+
+const fetchInfo = (handle: string) =>
+    Effect.runPromise(
+        doWork(handle)
+            .pipe(Effect.scoped, Effect.provide(FetchHttpClient.layer)),
+    );
 
 const Page: Component = () => {
     const navigate = useNavigate();
     const params = useParams<{ handle?: string | undefined; }>();
-    const [clearskyLists] = createResource(() => params.handle || undefined, fetchClearskyLists);
-    const [blueskyLists] = createResource(
-        () => clearskyLists.state === "ready" ? clearskyLists() : undefined,
-        fetchBlueskyLists,
-    );
-
+    const [info] = createResource(() => params.handle || undefined, fetchInfo);
     return (
         <div>
             <h1>Bluesky Moderation List Finder</h1>
@@ -99,37 +130,36 @@ const Page: Component = () => {
             </form>
 
             <Switch>
-                <Match when={clearskyLists.loading}>
-                    <p>Fetching lists from Clearsky...</p>
+                <Match when={info.loading}>
+                    <p>Loading...</p>
                 </Match>
-                <Match when={clearskyLists.error}>
-                    <span>Error: {`${clearskyLists.error}`}</span>
+                <Match when={info.error}>
+                    <span>Error: {`${info.error}`}</span>
                 </Match>
-                {
-                    /* <Match when={lists()}>
+                <Match when={info()}>
+                    <p>{info()!.profile.handle}</p>
+                    <p>{info()!.lists.length} moderation lists</p>
                     <ul>
-                        <For each={lists()}>
+                        <For each={info()!.lists}>
                             {(list) => (
                                 <li>
-                                    <a href={list.url}>{list.name}</a>
-                                    <p>{list.description}</p>
+                                    <a href={`https://bsky.app/profile/${list.profile.handle}`}>
+                                        {list.profile.handle}
+                                    </a>
+                                    <p>{list.list.name}</p>
+                                    <Show when={list.list.description}>
+                                        <p>{list.list.description}</p>
+                                    </Show>
                                 </li>
                             )}
                         </For>
                     </ul>
-                </Match> */
-                }
-            </Switch>
-
-            <Switch>
-                <Match when={blueskyLists.loading}>
-                    <p>Loading user info from Bluesky...</p>
-                </Match>
-                <Match when={blueskyLists.error}>
-                    <span>Error: {`${blueskyLists.error}`}</span>
-                </Match>
-                <Match when={blueskyLists()}>
-                    <p>{blueskyLists()?.length} users</p>
+                    <p>{info()!.blueskyErrors.length} errors</p>
+                    <ul>
+                        <For each={info()!.blueskyErrors}>
+                            {(error) => <li>{`${error}`}</li>}
+                        </For>
+                    </ul>
                 </Match>
             </Switch>
         </div>
